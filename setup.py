adefafe7ca92742652e14050e821958cccd7d53f
Víctor Cuervo <vcuervo@gmail.com>
2017-10-07 21:45:45 +0200

    Aplicación Flask de Test

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Test App',
+      version='1.0',
+      description='Aplicacion de Test',
+      author='Victor Cuervo',
+      author_email='vcuervo@gmail.com',
+      install_requires=['Flask==0.10.1']
+     )
