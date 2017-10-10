59c6345a239618dd8f80d127ce2d01167885b1de
Víctor Cuervo <vcuervo@gmail.com>
2017-10-11 01:43:23 +0200

    Nuevo modelo de servidor.

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ from flaskapp import app as application
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8080, application)
+    httpd = make_server('0.0.0.0', 8080, application)
     httpd.serve_forever()
