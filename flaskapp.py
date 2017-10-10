0ed84fa5a5dad75afdda3b32aeb3de53aaa358ac
Víctor Cuervo <vcuervo@gmail.com>
2017-10-11 01:44:20 +0200

    Nuevo contenido de Flaskapp

--- flaskapp.py
+++ flaskapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def holamundo():
-    return 'Hola soy un Test!'
+    return 'Hola soy un Test para OpenShift 3!'
 
 if __name__ == '__main__':
     app.run()
