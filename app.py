d6209bacfdd40ff38b037218b9570af74d66c44a
Víctor Cuervo <vcuervo@gmail.com>
2017-10-16 23:36:19 +0200

    Adaptado para mantenimiento.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def holamundo():
-    return 'Hola soy un Test para OpenShift 3!'
+    return 'Aplicación Verracos en Mantenimiento. Disculpen las molestias.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080')
